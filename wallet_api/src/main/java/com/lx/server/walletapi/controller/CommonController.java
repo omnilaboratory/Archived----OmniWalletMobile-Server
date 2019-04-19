231f1399e47e5ff216f37cc297ad28a9c7084f4e
ZhuGuojun <254698748@qq.com>
2019-04-19 14:19:33 +0800

    英文提示

--- wallet_api/src/main/java/com/lx/server/walletapi/controller/CommonController.java
+++ wallet_api/src/main/java/com/lx/server/walletapi/controller/CommonController.java
@@ -3,17 +3,12 @@ package com.lx.server.walletapi.controller;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSON;
 import com.lx.server.bean.ResultTO;
-import com.lx.server.enums.EnumKafkaTopic;
-import com.lx.server.kafka.bean.KafkaMessage;
 import com.lx.server.pojo.UserClient;
 import com.lx.server.service.UserClientService;
 import com.lx.server.utils.Tools;
@@ -29,18 +24,15 @@ public class CommonController extends AbstractController{
 	@Autowired
 	private UserClientService userClientService;
 	
-	@Autowired
-    private KafkaTemplate<String, Object> kafkaTemplate;
-	
 	@SuppressWarnings("serial")
 	@PostMapping("createUser")
 	@ApiOperation("创建新用户")
 	public ResultTO createUser(String userId,String nickname) {
 		this.logger.info("createUser");
-		Assert.isTrue(Tools.isValidMessageAudio(userId), "不正确的用户id");
+		Assert.isTrue(Tools.isValidMessageAudio(userId), "userId is not md5 type");
 		UserClient userClient = userClientService.createNewUser(userId,nickname);
 		if (userClient==null) {
-			return ResultTO.newFailResult("创建失败");
+			return ResultTO.newFailResult("create user fail");
 		}
 		
 		return ResultTO.newSuccessResult(new HashMap<String,Object>() {{
@@ -48,25 +40,20 @@ public class CommonController extends AbstractController{
 		}});
 	}
 	
+	
 	@SuppressWarnings("serial")
 	@PostMapping("restoreUser")
 	@ApiOperation("根据助记词恢复用户")
 	public ResultTO restoreUser(String userId) {
 		this.logger.info("restoreUser");
-		Assert.isTrue(Tools.isValidMessageAudio(userId), "不正确的用户id");
+		Assert.isTrue(Tools.isValidMessageAudio(userId), "userId is not md5 type");
 		UserClient userClient = userClientService.selectObject(userId);
 		if (userClient==null) {
-			return ResultTO.newFailResult("恢复失败");
+			return ResultTO.newFailResult("restoreUser fail");
 		}
 		return ResultTO.newSuccessResult(new HashMap<String,Object>() {{
 			put("userId", userClient.getId());
+			put("nickname", userClient.getNickname());
 		}});
-	}
-	
-	@GetMapping("testKafka")
-	@ApiOperation("测试kafka")
-	public ResultTO testKafka() {
-		kafkaTemplate.send(EnumKafkaTopic.UserTopic.value,JSON.toJSONString(new KafkaMessage(1,null,"msg",null)));
-		return ResultTO.newSuccessResult("恢复失败");
 	}
 }
